827dae46f5cf2eeeab3f68ba0e284349937d8528
Andrew-Maxwell <apmaxwell4@gmail.com>
2021-01-15 23:15:04 -0800

    Added opposite wall check

--- snacman2.cpp
+++ snacman2.cpp
@@ -11,8 +11,6 @@
 #define WIDTH 800
 #define HEIGHT 600
 #define GRID 40
-// tick rate is 
-#define TICK_RATE 10
 
 using namespace std;
 
@@ -77,10 +75,7 @@ struct mainData {
     list<segment> snake;
     list<spider> spiders;
     int snakeSize = 1;
-    int tickCount = 0;
     compass c;
-    Color colors[2] = {BLUE, YELLOW};
-    int color = 0;
 
     void readLevel(string levelName) {
         ifstream level(levelName);
@@ -101,54 +96,37 @@ struct mainData {
 
     void mainLoop() {
         BeginDrawing();
-        ClearBackground(BLACK);
-        // draw map
         for (int row = 0; row < map.size(); row++) {
             for (int col = 0; col < map[row].size(); col++) {
                 if (map[row][col] == '#') {
                     DrawRectangle(GRID * col, GRID * row, GRID, GRID, GRAY);
                 }
-                if (map[row][col] == 'A') {
-                    DrawCircle((col + 0.5) * GRID, (row + 0.5) * GRID, GRID / 2, RED);
-                }
             }
         }
-        // draw snake
         for (segment & s : snake) {
-            DrawCircle((s.pos.x + 0.5) * GRID, (s.pos.y + 0.5) * GRID, GRID / 2, colors[color % 2]);
+            DrawCircle((s.pos.x + 0.5) * GRID, (s.pos.y + 0.5) * GRID, GRID / 2, YELLOW);
         }
-        if (tickCount % TICK_RATE == 0) {
-            for (int i = -1; i < 3; i++) {
-                V2 nextForward = c.get(snake.begin()->forward, i);
-                V2 nextPos = snake.begin()->pos + nextForward;
-                V2 nextDown = c.get(nextForward, -1);
-                if (map[nextPos.y][nextPos.x] != '#') {
-                    // found apple, add a new segment
-                    if (map[nextPos.y][nextPos.x] == 'A') {
-                        snakeSize++;
-                        snake.push_back(segment(nextPos, nextForward, nextDown));
-                        // remove apple from map
-                        map[nextPos.y][nextPos.x] = ' ';
-                    }
-                    segment next(nextPos, nextForward, nextDown);
-                    snake.push_front(next);
-                    break;
-                }
-            }
-            while (snake.size() > snakeSize) {
-                snake.pop_back();
+        for (int i = -1; i < 3; i++) {
+            V2 nextForward = c.get(snake.begin()->forward, i);
+            V2 nextDown = c.get(nextForward, -1);
+            V2 nextPos = snake.begin()->pos + nextForward;
+            V2 nextWall = snake.begin()->pos + nextDown
+            if (map[nextPos.y][nextPos.x] != '#' && map) {
+                segment next(nextPos, nextForward, nextDown);
+                snake.push_front(next);
+                break;
             }
         }
         if (IsKeyPressed(KEY_SPACE)) {
-            V2 swapWall = snake.begin()->pos + c.get(snake.begin()->down, 2);
-            if (map[swapWall.y][swapWall.x] == '#') {
+            V2 nextWall = c.get(snake.begin()->down, 2);
+            if (snake.begin()->pos + nextWall == '#') {
                 c.reverse();
-                color++;
             }
         }
-
+        while (snake.size() > snakeSize) {
+            snake.pop_back();
+        }
         EndDrawing();
-        tickCount++;
     }
 
 };
@@ -171,7 +149,7 @@ int main(int argc, char** argv) {
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(doEverything, 6, 1);
 #else
-    SetTargetFPS(60);
+    SetTargetFPS(6);
     while (!WindowShouldClose()) {
         everything.mainLoop();
     }
