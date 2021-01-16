70b4743b5625e2424e170f851986383fe7e2ef09
Aidan Sojourner <aidan@sojourner.me>
2021-01-15 22:46:15 -0800

    Add apples, fix tick rate

--- snacman.cpp
+++ snacman.cpp
@@ -11,6 +11,8 @@
 #define WIDTH 800
 #define HEIGHT 600
 #define GRID 40
+// tick rate is 
+#define TICK_RATE 10
 
 using namespace std;
 
@@ -75,6 +77,7 @@ struct mainData {
     list<segment> snake;
     list<spider> spiders;
     int snakeSize = 1;
+    int tickCount = 0;
     compass c;
 
     void readLevel(string levelName) {
@@ -97,33 +100,49 @@ struct mainData {
     void mainLoop() {
         BeginDrawing();
         ClearBackground(BLACK);
+        // draw map
         for (int row = 0; row < map.size(); row++) {
             for (int col = 0; col < map[row].size(); col++) {
                 if (map[row][col] == '#') {
                     DrawRectangle(GRID * col, GRID * row, GRID, GRID, GRAY);
                 }
+                if (map[row][col] == 'A') {
+                    DrawCircle((col + 0.5) * GRID, (row + 0.5) * GRID, GRID / 2, RED);
+                }
             }
         }
+        // draw snake
         for (segment & s : snake) {
             DrawCircle((s.pos.x + 0.5) * GRID, (s.pos.y + 0.5) * GRID, GRID / 2, YELLOW);
         }
-        for (int i = -1; i < 3; i++) {
-            V2 nextForward = c.get(snake.begin()->forward, i);
-            V2 nextPos = snake.begin()->pos + nextForward;
-            if (map[nextPos.y][nextPos.x] != '#') {
+        if (tickCount % TICK_RATE == 0) {
+            for (int i = -1; i < 3; i++) {
+                V2 nextForward = c.get(snake.begin()->forward, i);
+                V2 nextPos = snake.begin()->pos + nextForward;
                 V2 nextDown = c.get(nextForward, -1);
-                segment next(nextPos, nextForward, nextDown);
-                snake.push_front(next);
-                break;
+                if (map[nextPos.y][nextPos.x] != '#') {
+                    // found apple, add a new segment
+                    if (map[nextPos.y][nextPos.x] == 'A') {
+                        snakeSize++;
+                        snake.push_back(segment(nextPos, nextForward, nextDown));
+                        // remove apple from map
+                        map[nextPos.y][nextPos.x] = ' ';
+                    }
+                    segment next(nextPos, nextForward, nextDown);
+                    snake.push_front(next);
+                    break;
+                }
+            }
+            while (snake.size() > snakeSize) {
+                snake.pop_back();
             }
         }
         if (IsKeyPressed(KEY_SPACE)) {
             c.reverse();
         }
-        while (snake.size() > snakeSize) {
-            snake.pop_back();
-        }
+
         EndDrawing();
+        tickCount++;
     }
 
 };
@@ -146,7 +165,7 @@ int main(int argc, char** argv) {
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(doEverything, 6, 1);
 #else
-    SetTargetFPS(6);
+    SetTargetFPS(60);
     while (!WindowShouldClose()) {
         everything.mainLoop();
     }
